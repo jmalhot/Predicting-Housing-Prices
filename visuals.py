682d01484c2ac196097a3d338f8a4644ec305899
Jatin Malhotra <jatinmalhotra@Jatins-MacBook-Pro.local>
2018-12-22 08:52:52 +0530

    pushing

--- visuals.py
+++ visuals.py
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
 # Display inline matplotlib plots with IPython
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
-###########################################
+############################################
 
 import matplotlib.pyplot as pl
 import numpy as np
